e71da25b2123cf63831d39d8bd03270f5dc603b5
Maurits van Rees <maurits@vanrees.org>
2015-11-21 12:29:14 +0100

    Preparing release 1.14
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.14 (unreleased)
+1.14 (2015-11-21)
 -----------------
 
 - Nothing changed yet.
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.14.dev0'
+version = '1.14'
 
 setup(name='mauritstestpackage',
       version=version,
