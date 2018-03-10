058deb4fefd40bd5e956f2b46c3062f84cf63f83
Maurits van Rees <maurits@vanrees.org>
2018-03-10 15:42:21 +0100

    Preparing release 1.24.16
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.24.16 (unreleased)
+1.24.16 (2018-03-10)
 --------------------
 
 - Nothing changed yet.
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.24.16.dev0'
+version = '1.24.16'
 
 setup(name='mauritstestpackage2',
       version=version,
