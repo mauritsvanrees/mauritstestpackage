d33bb6dbfc4754cc7a9b8a86a6a4fbb4005b571f
Maurits van Rees <maurits@vanrees.org>
2022-12-09 21:57:46 +0100

    Preparing release 3.0.0a12
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-3.0.0a12 (unreleased)
+3.0.0a12 (2022-12-09)
 ---------------------
 
 - Nothing changed yet.
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = "3.0.0a12.dev0"
+version = "3.0.0a12"
 
 setup(
     name="mauritstestpackage2",
