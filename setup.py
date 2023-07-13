81ae92482b5a1e91cb03630c8e8c603493492675
Maurits van Rees <maurits@vanrees.org>
2023-07-13 14:44:47 +0200

    python_requires

--- setup.py
+++ setup.py
@@ -15,8 +15,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.11",
     ],
@@ -28,6 +26,7 @@ setup(
     packages=find_namespace_packages(),
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.8",
     install_requires=[
     ],
     entry_points="""
